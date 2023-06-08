83e221f6a561663e088b077469f4436f43938c62
jhosuapp <jhosuapvll@gmail.com>
2023-06-08 09:12:34 -0500

    Updates

---
+++ js/mainscript.umd.cjs
@@ -0,0 +1,2 @@
+(function(n){typeof define=="function"&&define.amd?define(n):n()})(function(){"use strict";const n=(()=>{const t=()=>{console.log("funci\xF3n ejecutadaa")};return{childsTest:function(){try{t()}catch(s){}}}})(),e=()=>{n.childsTest()},i="";window.addEventListener("load",()=>{e()})});
+//# sourceMappingURL=mainscript.umd.cjs.map
